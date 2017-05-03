2a931da3da0304763dd20400eca2522b7b0f1881
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2017-05-03 15:26:26 +0200

    different case test

--- test/test.py
+++ test/test.py
@@ -7,7 +7,9 @@ c=discover_docker.DDOSAWorkerContainer()
 r=dc.RemoteDDOSA(c.url)\
         .query(target="ii_skyimage",
                modules=["ddosa","git://ddosadm"],
-               assume=['ddosa.ScWData(input_scwid="066500220010.001")'])
+               assume=['ddosa.ScWData(input_scwid="066500220010.001")',
+                       'ddosa.ImageBins(use_ebins=[(20,40)],use_version="onebin_20_40")',
+                       'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'])
 
 print r["result"]
 
@@ -16,3 +18,13 @@ print r.keys()
 data=ast.literal_eval(str(r['data']))
         
 json.dump(data,open("data.json","w"), sort_keys=True, indent=4, separators=(',', ': '))
+print "jsonifiable data dumped to data.json"
+
+print "cached object in",r['cached_path']
+
+for k,v in data.items():
+    try:
+        if v[0]=="DataFile":
+            print "data file attached:",k,(r['cached_path'][0].replace("data/ddcache",c.ddcache_root)+"/"+v[1]).replace("//","/")
+    except:
+        pass
