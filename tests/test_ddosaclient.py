d20c835e579c26b07dbae6e6ef1a61ea8f4dc18a
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-10-27 17:44:20 +0200

    exceptions by case

--- tests/test_ddosaclient.py
+++ tests/test_ddosaclient.py
@@ -31,7 +31,7 @@ def test_failing_request():
 
     #with pytest.raises(requests.ConnectionError):
 
-    with pytest.raises(ddosaclient.WorkerException):
+    with pytest.raises(ddosaclient.AnalysisException):
         product=remote.query(target="FailingMedia",
                              modules=["ddosa","git://ddosadm"],
                              assume=[scwsource_module+'.ScWData(input_scwid="035200250010.001")',
@@ -44,7 +44,7 @@ def test_bad_request():
 
     #with pytest.raises(requests.ConnectionError):
 
-    with pytest.raises(ddosaclient.WorkerException):
+    with pytest.raises(ddosaclient.AnalysisException):
         product=remote.query(target="Undefined",
                              modules=["ddosa","git://ddosadm"],
                              assume=[scwsource_module+'.ScWData(input_scwid="035200250010.001")',
@@ -54,24 +54,24 @@ def test_bad_request():
 def test_graph_exception():
     remote=ddosaclient.AutoRemoteDDOSA()
 
-    product=remote.query(target="CatExtract",
+    with pytest.raises(ddosaclient.AnalysisException):
+        product=remote.query(target="CatExtract",
                          modules=["ddosa","git://ddosadm"],
                          assume=['ddosa.ImageBins(use_ebins=[(20,40)],use_version="onebin_20_40")',
                                  'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'])
 
-    print("product:",product)
 
 
 def test_cat_exception():
     remote=ddosaclient.AutoRemoteDDOSA()
 
-    product=remote.query(target="CatExtract",
+    with pytest.raises(ddosaclient.AnalysisException):
+        product=remote.query(target="CatExtract",
                          modules=["git://ddosa","git://ddosadm"],
                          assume=[scwsource_module+'.ScWData(input_scwid="935200230010.001")',
                                  'ddosa.ImageBins(use_ebins=[(20,40)],use_version="onebin_20_40")',
                                  'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'])
 
-    print("product:",product)
 
 def test_cat():
     remote=ddosaclient.AutoRemoteDDOSA()
@@ -321,28 +321,6 @@ def test_mosaic():
     remote=ddosaclient.AutoRemoteDDOSA()
 
     product=remote.query(target="Mosaic",
-              modules=["ddosa","git://ddosadm","git://osahk","git://mosaic",'git://rangequery'],
-              assume=['mosaic.ScWImageList(\
-                  input_scwlist=\
-                  rangequery.TimeDirectionScWList(\
-                      use_coordinates=dict(RA=83,DEC=22,radius=5),\
-                      use_timespan=dict(T1="2008-04-12T11:11:11",T2="2009-04-12T11:11:11"),\
-                      use_max_pointings=50 \
-                      )\
-                  )\
-              ',
-              'mosaic.Mosaic(use_pixdivide=4)',
-              'ddosa.ImageBins(use_ebins=[(20,40)],use_version="onebin_20_40")',
-              'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'])
-
-
-    assert os.path.exists(product.skyima)
-
-
-def test_mosaic():
-    remote=ddosaclient.AutoRemoteDDOSA()
-
-    product=remote.query(target="TimeDirectionScWList",
               modules=["ddosa","git://ddosadm","git://osahk","git://mosaic",'git://rangequery'],
               assume=['mosaic.ScWImageList(\
                   input_scwlist=\
