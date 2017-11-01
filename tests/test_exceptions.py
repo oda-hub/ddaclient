166a000e450c56101410eaade961b89c40dfc71b
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-11-01 15:25:25 +0100

    empty scwlist exception case

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -80,3 +80,130 @@ def test_handled_exception():
         assert len(e.exceptions)==1
         print(e.exceptions)
         assert e.exceptions[0]['node']=="ScWData"
+
+def test_mosaic_exception_empty():
+    remote=ddosaclient.AutoRemoteDDOSA()
+
+    try:
+        product=remote.query(target="Mosaic",
+              modules=["ddosa","git://ddosadm","git://osahk","git://mosaic",'git://rangequery'],
+              assume=['mosaic.ScWImageList(\
+                  input_scwlist=\
+                  rangequery.TimeDirectionScWList(\
+                      use_coordinates=dict(RA=83,DEC=22,radius=5),\
+                      use_timespan=dict(T1="2010-04-12T11:11:11",T2="2009-04-12T11:11:11"),\
+                      use_max_pointings=50 \
+                      )\
+                  )\
+              ',
+              'mosaic.Mosaic(use_pixdivide=4)',
+              'ddosa.ImageBins(use_ebins=[(20,40)],use_version="onebin_20_40")',
+              'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'])
+    except ddosaclient.AnalysisException as e:
+        print(e)
+        assert hasattr(e, 'exceptions')
+        assert len(e.exceptions) == 1
+        print(e.exceptions)
+        assert e.exceptions[0]['exception']==u'EmptyScWList()'
+
+
+
+
+def test_mosaic_ii_exception_empty():
+    remote=ddosaclient.AutoRemoteDDOSA()
+
+    try:
+        product=remote.query(target="mosaic_ii_skyimage",
+              modules=["ddosa","git://ddosadm","git://osahk","git://mosaic",'git://rangequery'],
+              assume=['ddosa.ImageGroups(\
+                  input_scwlist=\
+                  rangequery.TimeDirectionScWList(\
+                      use_coordinates=dict(RA=83,DEC=22,radius=5),\
+                      use_timespan=dict(T1="2010-04-12T11:11:11",T2="2009-04-12T11:11:11"),\
+                      use_max_pointings=50 \
+                      )\
+                  )\
+              ',
+              'ddosa.ImageBins(use_ebins=[(20,40)],use_version="onebin_20_40")',
+              'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'])
+
+        assert os.path.exists(product.skyima)
+
+    except ddosaclient.AnalysisException as e:
+        print(e)
+        assert hasattr(e, 'exceptions')
+        assert len(e.exceptions) == 1
+        print(e.exceptions)
+        assert e.exceptions[0]['exception']==u'EmptyScWList()'
+
+
+
+
+def test_sum_spectrum_empty():
+    remote = ddosaclient.AutoRemoteDDOSA()
+
+    try:
+        product = remote.query(target="ISGRISpectraSum",
+                               modules=["ddosa", "git://ddosadm", "git://useresponse", "git://process_isgri_spectra",
+                                        "git://rangequery"],
+                               assume=['process_isgri_spectra.ScWSpectraList(\
+                      input_scwlist=\
+                      rangequery.TimeDirectionScWList(\
+                          use_coordinates=dict(RA=83,DEC=22,radius=5),\
+                          use_timespan=dict(T1="2011-04-12T11:11:11",T2="2009-04-12T11:11:11"),\
+                          use_max_pointings=3 \
+                          )\
+                      )\
+                  ',
+                                       'ddosa.ImageBins(use_ebins=[(20,40)],use_version="onebin_20_40")',
+                                       'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'])
+        import astropy.io.fits as fits
+        assert fits.open(product.isgri_sum_Crab)[1].header['EXPOSURE'] > 3000
+        # assert os.path.exists(product.spectrum)
+
+    except ddosaclient.WorkerException as e:
+        if len(e.args) > 2:
+            print(e[2])
+        raise
+    except ddosaclient.AnalysisException as e:
+        print(e)
+        assert hasattr(e, 'exceptions')
+        assert len(e.exceptions) == 1
+        print(e.exceptions)
+        assert e.exceptions[0]['exception']==u'EmptyScWList()'
+
+
+def test_lc_pick_empty():
+    remote = ddosaclient.AutoRemoteDDOSA()
+
+    try:
+        product = remote.query(target="lc_pick",
+                               modules=["ddosa", "git://ddosadm", "git://useresponse", "git://process_isgri_spectra",
+                                        "git://rangequery"],
+                               assume=['ddosa.LCGroups(\
+                      input_scwlist=\
+                      rangequery.TimeDirectionScWList(\
+                          use_coordinates=dict(RA=83,DEC=22,radius=5),\
+                          use_timespan=dict(T1="2011-04-12T11:11:11",T2="2009-04-12T11:11:11"),\
+                          use_max_pointings=3 \
+                          )\
+                      )\
+                  ',
+                                       'ddosa.ImageBins(use_ebins=[(20,40)],use_version="onebin_20_40")',
+                                       'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'])
+        import astropy.io.fits as fits
+        assert fits.open(product.isgri_sum_Crab)[1].header['EXPOSURE'] > 3000
+        # assert os.path.exists(product.spectrum)
+
+    except ddosaclient.WorkerException as e:
+        if len(e.args) > 2:
+            print(e[2])
+        raise
+    except ddosaclient.AnalysisException as e:
+        print(e)
+        assert hasattr(e, 'exceptions')
+        assert len(e.exceptions) == 1
+        print(e.exceptions)
+        assert e.exceptions[0]['exception']==u'EmptyScWList()'
+
+
