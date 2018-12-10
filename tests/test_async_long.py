16f72d04e7af0953eea88aa12d59842a288cc434
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2018-12-10 15:32:47 +0100

    more larger tests

--- tests/test_async_long.py
+++ tests/test_async_long.py
@@ -86,6 +86,87 @@ def test_mosaic_delegation_cat_distribute():
     assert 'NEW_1' in sr['NAME']
     assert 'TEST_SOURCE1' in sr['NAME']
         
+def test_mosaic_parallel():
+    remote=ddosaclient.AutoRemoteDDOSA()
+
+    random_ra=83+(random.random()-0.5)*5
+    cat = ['SourceCatalog',
+           {
+               "catalog": [
+                   {
+                       "DEC": 23,
+                       "NAME": "TEST_SOURCE1",
+                       "RA": 83
+                   },
+                   {
+                       "DEC": 13,
+                       "NAME": "TEST_SOURCE2",
+                       "RA": random_ra
+                   }
+               ],
+               "version": "v1"
+           }
+        ]
+
+
+
+
+        
+    kwargs_sets = {}
+    
+    for sub_version in range(3):
+        job_id=time.strftime("%y%m%d_%H%M%S")+"_%i"%sub_version
+        encoded=urllib.urlencode(dict(job_id=job_id,session_id="test_mosaic"))
+        print("encoded:",encoded)
+        custom_version = "imgbins_for_"+job_id
+        kwargs_sets[custom_version]=dict(target="mosaic_ii_skyimage",
+                           modules=["git://ddosa", 'git://rangequery','git://gencat/dev','git://ddosa_delegate'],
+                           assume=['ddosa.ImageGroups(input_scwlist=rangequery.TimeDirectionScWList)',
+                                   'rangequery.TimeDirectionScWList(\
+                                         use_coordinates=dict(RA=83,DEC=22,radius=5),\
+                                         use_timespan=dict(T1="2014-04-12T11:11:11",T2="2015-04-12T11:11:11"),\
+                                         use_max_pointings=%i \
+                                    )'%int(os.environ.get("TEST_N_POINTINGS",3)),
+                                   'ddosa.ImageBins(use_ebins=[(20,80)],use_autoversion=False, use_version="%s")'%custom_version,
+                                   'ddosa.ImagingConfig(use_SouFit=0,use_version="soufit0")'],
+                           callback=default_callback+"?"+encoded,
+                           prompt_delegate=True,
+                           inject=[cat],
+                         )
+    
+    statuses = {}
+
+    for v, kwargs in kwargs_sets.items():
+        with pytest.raises(ddosaclient.AnalysisDelegatedException) as excinfo:
+            product = remote.query(**kwargs)
+        statuses[v]=False
+        time.sleep(5)
+            # callback="http://intggcn01:5000/callback?job_id=1&asdsd=2",
+
+    assert excinfo.value.delegation_state == "submitted"
+
+    while not all(statuses.values()):
+        time.sleep(5)
+    
+        for v, kwargs in kwargs_sets.items():
+            try:
+                product = remote.query(**kwargs)
+            except ddosaclient.AnalysisDelegatedException as e:
+                print("state:",e.delegation_state)
+            else:
+                print("DONE:",product)
+                statuses[v]=True
+
+                assert hasattr(product,'skyima')
+                assert hasattr(product,'skyres')
+                assert hasattr(product,'srclres')
+
+                sr = fits.open(product.srclres)[1].data
+
+                print(sr)
+
+                assert 'NEW_1' in sr['NAME']
+                assert 'TEST_SOURCE1' in sr['NAME']
 
 
 def test_mosaic_delegation_failing():
