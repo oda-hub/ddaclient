8138610d32a2436ef5f04e280882eb6aa8eb1f28
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-05-06 10:25:50 +0200

    description

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+setup_requires = ['setuptools >= 30.3.0', 'setuptools-git-version']
+
 setup(
         name='ddaclient',
         version='1.0',
@@ -21,3 +23,9 @@ setup(
         description="client for data-analysis services",
         long_description=open('README.md').read(),
         )
+
+
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires.append('pytest-runner')
+
+
