0e33cdae3a272daf774109d2e904953187c5e9ce
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-02-14 15:39:05 +0100

    Bump version: 1.1.8 → 1.1.9-dev0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(
         name='ddaclient',
-        version='1.1.8',
+        version='1.1.9-dev0',
         py_modules= ['ddaclient','simple_logger'],
         package_data     = {
             "": [
