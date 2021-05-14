3345e6cc469980236660279ccbd0b718f6633c17
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-05-14 11:36:05 +0200

    Bump version: 1.1.12-dev0 → 1.1.12

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(
         name='ddaclient',
-        version='1.1.12-dev0',
+        version='1.1.12',
         py_modules= ['ddaclient','simple_logger'],
         package_data     = {
             "": [
